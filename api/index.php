1abdfa0b9b3f0a5e0b15a9d83f62ad50ce0a1556
Bagas Pamungkas <pamungkasbagas222@gmail.com>
2026-07-28 21:12:03 +0700

    Enable PHP error display in api/index.php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Forward request to Laravel public index.php
 require __DIR__ . '/../public/index.php';
